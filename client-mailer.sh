1a4d43a6795090c0092ceea0036e5df242393193
Evgeniy Baranov <master@kompolom.ru>
2014-03-30 00:13:03 +0400

    Определяем папку проектов на ЯД и создаем, если ее нет

--- client-mailer.sh
+++ client-mailer.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+########################
+# Yandex.Disk          #
+########################
+yadiskDir=$HOME/Yandex.Disk
+yadiskProjectsDir=$yadiskDir/projects
+
+echo "Каталог проектов: $yadiskProjectsDir"
+
+function mkdir_if_not_exists(){
+  dir=$1
+  if ! [ -d $dir ]; then
+    echo "Создаем каталог $dir"
+    if ! [ `mkdir $dir` ];then
+      exit 1
+    fi
+  fi
+}
+
+#Создает папку проектов, если ее еще нет
+mkdir_if_not_exists $yadiskProjectsDir
