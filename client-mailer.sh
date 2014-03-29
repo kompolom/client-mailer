2731df9843c631054ad20d49dca03f3cd32dbbac
Evgeniy Baranov <master@kompolom.ru>
2014-03-30 02:27:14 +0400

    Жирное выделение ссылки

--- client-mailer.sh
+++ client-mailer.sh
@@ -65,6 +65,6 @@ mv $ARCHIVE_FILE $TARGET_CATALOG
 
 #Получаем ссылку на архив
 PUBLIC_LINK=`yandex-disk publish $TARGET_CATALOG/$ARCHIVE_FILE`
-echo "Ссылка на скачивание: $PUBLIC_LINK"
+echo -e "Ссылка на скачивание: \033[1m$PUBLIC_LINK\033[0m"
 exit 0
 
